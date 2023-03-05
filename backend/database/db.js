30f2b1bfeff4e5469badfa6437e9a1733d129c35
amit369 <amitchaman369@gmail.com>
2023-03-05 13:53:55 +0530

    changes done

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@cluster0.inntzse.mongodb.net/?retryWrites=true&w=majority`;
+     try{
+       await mongoose.connect(URL, { useUnifiedTopology : true , useNewUrlParser : true});
+       console.log('Database Connected Successfully');
+     }  
+     catch(error)
+     {
+        console.log('Error while connecting with the database ', error );
+     }   
+}
+
+export default Connection;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
+import express from 'express';
+import Connection from './database/db.js';
+import dotenv from 'dotenv';
+import Routes from './routes/route.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 const app = express();
-
-app.get('/', (req,res)=> {
-    console.log("In the App Folder");
-    res.send("Hello World");
-})
-
-app.listen(5000);
+const PORT = 8000;
+dotenv.config();
+app.use('/',Routes);
+app.use(cors());
+app.use(bodyParser.json({ extended : true}));
+app.use(bodyParser.urlencoded({ extended : true}));
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+Connection(username, password);
+app.listen(PORT, () => {
+    console.log(`Server is running successfully on PORT ${PORT}`);
+});
